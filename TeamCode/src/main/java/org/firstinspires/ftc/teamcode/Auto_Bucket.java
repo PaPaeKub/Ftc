8b0c7ca287f37010c7c2915590cd757ec6196164
Katawaredoi <teammy1986@gmail.com>
2024-11-17 18:25:35 +0700

    10/11/2024**

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Bucket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Bucket.java
@@ -19,14 +19,14 @@ public class Auto_Bucket extends Robot {
 
         SetServoPos(0, G);
         sleep(300);
+        SetServoPos(0, AG);
         SetServoPos(1, LAG, RAG);
         SetServoPos(0, LFA, RFA);
         sleep(500);
         SetServoPos(0, LA, RA);
-        SetServoPos(0, AG);
         sleep(300);
         SetServoPos(0, D);
-        sleep(400);
+        sleep(300);
         SetServoPos(0.2, G);
         sleep(100);
         SetServoPos(0, LAG, RAG);
@@ -81,42 +81,42 @@ public class Auto_Bucket extends Robot {
 
         if (opModeIsActive()) {
             move(1.0, -0.77, 0.7, 218.0, new double[]{0.45, 0.01, 0.1},
-                    new double[]{2.5, 0.001, 0.2, 0.0}, new double[]{0.22, 0.0001, 0.015, 0.0}, new double[]{0.25, 0.008, 0.01, 0.0}, 0, High_Basket);
+                    new double[]{2.5, 0.001, 0.2, 0.0}, new double[]{0.25, 0.00001, 0.01, 0.0}, new double[]{0.25, 0.001, 0.01, 0.0}, 0.0001, High_Basket);
 
             drop();
 
             move(1.0, -0.7, 0.7, 188.0, new double[]{0.35, 0.004, 0.01},
-                    new double[]{2.0, 0.002, 0.15, 0.0}, new double[]{0.25, 0.02, 0.025, 0.0}, new double[]{0.35, 0.005, 0.01, 0.0}, 0, 0);
+                    new double[]{2.0, 0.002, 0.15, 0.0}, new double[]{0.25, 0.02, 0.025, 0.0}, new double[]{0.35, 0.005, 0.01, 0.0}, 0.0001, 0);
 
             keep(0);
 
             move(1.0, -0.77, 0.7, 218.0, new double[]{0.45, 0.001, 0.06},
-                    new double[]{1.0, 0.0008, 0.1, 0.0}, new double[]{0.1, 0.08, 0.015, 0.0}, new double[]{0.15, 0.01, 0.01, 0.0}, 0, High_Basket);
+                    new double[]{1.0, 0.0008, 0.1, 0.0}, new double[]{0.1, 0.08, 0.015, 0.0}, new double[]{0.15, 0.01, 0.01, 0.0}, 0.0001, High_Basket);
 
             drop();
 
             move(1.0, -0.98, 0.7, 180.0, new double[]{0.35, 0.004, 0.01},
-                    new double[]{2.0, 0.002, 0.15, 0.0}, new double[]{0.25, 0.01, 0.025, 0.0}, new double[]{0.35, 0.001, 0.01, 0.0}, 0, 0);
+                    new double[]{2.0, 0.002, 0.15, 0.0}, new double[]{0.25, 0.01, 0.025, 0.0}, new double[]{0.32, 0.001, 0.01, 0.0}, 0.0001, 0);
 
             keep(0);
 
             move(1.0, -0.77, 0.7, 218.0, new double[]{0.45, 0.001, 0.06},
-                    new double[]{1.0, 0.0008, 0.1, 0.0}, new double[]{0.1, 0.08, 0.015, 0.0}, new double[]{0.15, 0.01, 0.01, 0.0}, 0, High_Basket);
+                    new double[]{1.0, 0.0008, 0.1, 0.0}, new double[]{0.1, 0.08, 0.015, 0.0}, new double[]{0.15, 0.01, 0.01, 0.0}, 0.0001, High_Basket);
 
             drop();
 
             move(1.0, -1.0, 0.83, 150.0, new double[]{0.3, 0.0009, 0.008},
-                    new double[]{2.0, 0.002, 0.05, 0.0}, new double[]{0.15, 0.007, 0.0009, 0.0}, new double[]{0.3, 0.008, 0.01, 0.0}, 0, 0);
+                    new double[]{2.0, 0.002, 0.05, 0.0}, new double[]{0.15, 0.007, 0.0009, 0.0}, new double[]{0.3, 0.008, 0.01, 0.0}, 0.0001, 0);
 
             keep(0.15);
 
             move(1.0, -0.77, 0.7, 218.0, new double[]{0.45, 0.001, 0.06},
-                    new double[]{1.0, 0.0008, 0.1, 0.0}, new double[]{0.08, 0.009, 0.015, 0.0}, new double[]{0.15, 0.01, 0.01, 0.0}, 0, High_Basket);
+                    new double[]{1.0, 0.0008, 0.1, 0.0}, new double[]{0.08, 0.009, 0.015, 0.0}, new double[]{0.15, 0.001, 0.01, 0.0}, 0.0001, High_Basket);
 
             drop();
 
             move(1.0, 0.3, 2.1, 90.0, new double[]{0.45, 0.05, 0.08},
-                    new double[]{4.0, 0.01, 0.8, 0.0}, new double[]{0.25, 0.01, 0.0095, 0.0}, new double[]{0.35, 0.01, 0.01, 0.0}, 0, 800);
+                    new double[]{4.0, 0.01, 0.8, 0.0}, new double[]{0.25, 0.01, 0.0095, 0.0}, new double[]{0.35, 0.01, 0.01, 0.0}, 0.0001, 800);
 
             SetServoPos(0.59, LLG, RLG);
 
